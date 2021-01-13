cf79ab7282a9684b9f2dceec50fbe8f6467030a1
Dracule Mihawk <77284982+HawkEye-Mihawk@users.noreply.github.com>
2021-01-13 19:55:29 +0530

    Create Revising the Select Query I
    Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.

---
+++ Revising the Select Query I.sql
@@ -0,0 +1,3 @@
+SELECT *     -- for selecting everything from the table
+FROM CITY        -- table CITY selected
+WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000     -- conditions given where COUNTRYCODE should match to USA and POPULATION should be greater than 100000
